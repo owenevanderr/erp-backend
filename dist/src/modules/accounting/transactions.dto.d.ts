feb69e3c05d15c012f282265fc88e85cf1e4554e
owenevanderr <111734751+owenevanderr@users.noreply.github.com>
2025-10-14 08:37:26 -0700

    remove dist folder

--- dist/src/modules/accounting/transactions.dto.d.ts
+++
@@ -1,7 +0,0 @@
-export declare class CreateTransactionDto {
-    type: 'CASH_IN' | 'CASH_OUT';
-    amount: number;
-    description?: string;
-    customerId?: string;
-    supplierId?: string;
-}
--- dist/src/modules/crm/crm.controller.d.ts
+++
@@ -1,5 +0,0 @@
-export declare class CrmController {
-    reminders(): {
-        reminders: never[];
-    };
-}
